2ef70cb26b97649a9eae1ff297951f979e4ceb41
Eliezer_dlc <148479015+Eliezer095@users.noreply.github.com>
2025-03-03 11:39:31 -0600

    cambios

--- Nucleo/views.py
+++ Nucleo/views.py
@@ -2,7 +2,7 @@ from django.views.generic import View
 from django.shortcuts import render
 
 class HomeViews(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         context={
 
         }

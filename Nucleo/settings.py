c5de2e32fe479d2cdc6252d80d056b78961738f2
Eliezer_dlc <148479015+Eliezer095@users.noreply.github.com>
2025-03-04 11:13:53 -0600

    3.9

--- Nucleo/settings.py
+++ Nucleo/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIR = (os.path.join(BASE_DIR,"Nucleo/static"),)
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
